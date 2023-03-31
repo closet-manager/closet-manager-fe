64cd07162682bab5e4d12140ea07c5d0f0ac8a4d
Blanche Haddad <blancherachel@gmail.com>
2023-03-31 10:49:06 -0600

    feat: Add menuPosition prop and type for selects

--- src/Components/Closet/Closet.tsx
+++ src/Components/Closet/Closet.tsx
@@ -18,6 +18,12 @@ interface Item {
   attributes: attributes;
 }
 
+declare module 'react' {
+  interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+    menuPosition?: string;
+  }
+}
+
 export const Closet = (): JSX.Element => {
   const [allItems, setAllItems] = useState<Item[]>([]);
   const [filteredItems, setFilteredItems] = useState<Item[]>([]); 
@@ -89,7 +95,7 @@ export const Closet = (): JSX.Element => {
     <div className="closet-container">
       <h2>My Closet</h2>
       <div id="filter" onChange={handleFilter}>
-        <select id="filter--clothing-type" name="type">
+        <select id="filter--clothing-type" name="type" menuPosition="fixed" className="select">
           <option value="">Clothing Type</option>
           <option value="tops">Tops</option>
           <option value="bottoms">Bottoms</option>
@@ -98,27 +104,29 @@ export const Closet = (): JSX.Element => {
           <option value="accessories">Accessories</option>
           <option value="other">Other</option>
         </select>
-        <select id="filter--color" name="color">
-          <option value="">Color</option>
-          <option value="red">Red</option>
-          <option value="orange">Orange</option>
-          <option value="yellow">Yellow</option>
-          <option value="green">Green</option>
-          <option value="blue">Blue</option>
-          <option value="purple">Purple</option>
-          <option value="black">Black</option>
-          <option value="white">White</option>
-          <option value="neutral">Neutral</option>
-          <option value="other">Multi</option>
-        </select>
-        <select id="filter--season" name="season">
+        <div className="select-container">
+          <select id="filter--color" name="color" menuPosition="fixed" className="select">
+            <option value="">Color</option>
+            <option value="red">Red</option>
+            <option value="orange">Orange</option>
+            <option value="yellow">Yellow</option>
+            <option value="green">Green</option>
+            <option value="blue">Blue</option>
+            <option value="purple">Purple</option>
+            <option value="black">Black</option>
+            <option value="white">White</option>
+            <option value="neutral">Neutral</option>
+            <option value="other">Multi</option>
+          </select>
+        </div>
+        <select id="filter--season" name="season" menuPosition="fixed" className="select">
           <option value="">Season</option>
           <option value="fall">Fall</option>
           <option value="winter">Winter</option>
           <option value="spring">Spring</option>
           <option value="summer">Summer</option>
         </select>
-        <select id="filter--favorite" name="favorite">
+        <select id="filter--favorite" name="favorite" menuPosition="fixed" className="select">
           <option value="">See All</option>
           <option value="favorites">Only Favorites</option>
         </select>
