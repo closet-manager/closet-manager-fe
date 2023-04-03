a6edf73d86f239929881b4229419c2345fbae07c
Karrar Qasim <Karawr1996@gmail.com>
2023-04-03 12:27:27 -0600

    Add selectedlist to keep track of which state is currently being selected and added back button

--- src/Components/MyLists/MyLists.tsx
+++ src/Components/MyLists/MyLists.tsx
@@ -1,20 +1,29 @@
 import { useEffect, useState } from 'react';
+import { List } from '../List/List'
 import './MyLists.css'
 
-interface ListItem {
+interface ListDetails {
   id: number;
   name: string;
+  items: string[];
+}
+
+interface ListProps {
+  listId: number;
+  onBack: () => void;
 }
 
 export const MyLists: React.FC<{ userId: number }> = ({ userId }) => {
-  const [lists, setLists] = useState<ListItem[]>([]);
+  const [lists, setLists] = useState<ListDetails[]>([]); // change state type to ListDetails[]
+  const [selectedList, setSelectedList] = useState<number | null>(null); // track which list is selected
   const [error, setError] = useState<string | null>(null);
 
   const fetchLists = async () => {
     try {
       const response = await fetch(`https://closet-manager-be.herokuapp.com/api/v1/users/${userId}/lists`);
       const data = await response.json();
-      setLists(data.data.map((list:any) => ({ id: parseInt(list.id), name: list.attributes.name })));
+      const listDetails = data.data.map((list: any) => ({ id: parseInt(list.id), name: list.attributes.name, items: list.attributes.items }));
+      setLists(listDetails);
     } catch (error) {
       console.error(error);
       setError('An error occurred while fetching the lists.');
@@ -26,21 +35,31 @@ export const MyLists: React.FC<{ userId: number }> = ({ userId }) => {
   }, [userId]);
 
   const handleListClick = (listId: number) => {
-    console.log(`List with ID ${listId} clicked`);
+    setSelectedList(listId); // set the selectedList state to the ID of the clicked list
+  };
+
+  const handleBackClick = () => {
+    setSelectedList(null); // set the selectedList state back to null when the user goes back
   };
 
   return (
     <div>
-      <h2>Custom Lists</h2>
-      {error ? (
-        <h2>{error}</h2>
+      {selectedList ? ( // if a list is selected, show the List component
+        <List listId={selectedList} onBack={handleBackClick} />
       ) : (
-        <div className="button-container">
-          {lists.map((list) => (
-            <button key={list.id} className="list-button" onClick={() => handleListClick(list.id)}>
-              {list.name}
-            </button>
-          ))}
+        <div>
+          <h2>Custom Lists</h2>
+          {error ? (
+            <h2>{error}</h2>
+          ) : (
+            <div className="button-container">
+              {lists.map((list) => (
+                <button key={list.id} className="list-button" onClick={() => handleListClick(list.id)}>
+                  {list.name}
+                </button>
+              ))}
+            </div>
+          )}
         </div>
       )}
     </div>
