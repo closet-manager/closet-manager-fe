70738f6aa5f3f6cc83f26d62caaf551983935c3c
Karrar Qasim <Karawr1996@gmail.com>
2023-04-03 11:22:38 -0600

    Add state

--- src/Components/List/List.tsx
+++ src/Components/List/List.tsx
@@ -11,3 +11,8 @@ interface ListProps {
   listId: number;
   onBack: () => void;
 }
+
+export const List: React.FC<ListProps> = ({ listId, onBack }) => {
+  const [listDetails, setListDetails] = useState<ListDetails>({ id: 0, name: '', items: [] });
+  const [error, setError] = useState<string | null>(null);
+}
