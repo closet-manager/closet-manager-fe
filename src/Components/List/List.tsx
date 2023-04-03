d865b1c21727e5c2822a69005af6e2f42e9066ed
Karrar Qasim <Karawr1996@gmail.com>
2023-04-03 11:35:09 -0600

    Add fetchcall for list details

--- src/Components/List/List.tsx
+++ src/Components/List/List.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import React from "react";
+import './List.css';
 
 interface ListDetails {
   id: number;
@@ -15,4 +16,21 @@ interface ListProps {
 export const List: React.FC<ListProps> = ({ listId, onBack }) => {
   const [listDetails, setListDetails] = useState<ListDetails>({ id: 0, name: '', items: [] });
   const [error, setError] = useState<string | null>(null);
-}
+
+  const fetchListDetails = async () => {
+    try {
+      const response = await fetch(`https://closet-manager-be.herokuapp.com/api/v1/lists/${listId}`);
+      const data = await response.json();
+      const list = data.data;
+      setListDetails({ id: parseInt(list.id), name: list.attributes.name, items: list.attributes.items });
+    } catch (error) {
+      console.error(error);
+      setError('An error occurred while fetching the list details.');
+    }
+  };
+
+  useEffect(() => {
+    fetchListDetails();
+  }, [listId]);
+
+};
