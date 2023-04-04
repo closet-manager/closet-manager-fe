d8bd7b8a2edfa86f12cc875d2da8ecd8248e1727
Karrar Qasim <Karawr1996@gmail.com>
2023-04-04 15:54:55 -0600

    Oorganized code in list

--- src/Components/List/List.tsx
+++ src/Components/List/List.tsx
@@ -1,9 +1,8 @@
-import { useState, useEffect } from 'react';
-import React from "react";
-import { Card } from '../Card/Card';
-import './List.css';
+import React, { useState, useEffect } from "react";
+import { Card } from "../Card/Card";
+import "./List.css";
 
-interface attributes {
+interface Attributes {
   season: string;
   clothing_type: string;
   size: string;
@@ -15,7 +14,7 @@ interface attributes {
 interface Item {
   id: string;
   type: string;
-  attributes: attributes;
+  attributes: Attributes;
 }
 
 interface ListProps {
@@ -24,25 +23,27 @@ interface ListProps {
 }
 
 export const List: React.FC<ListProps> = ({ listId, onBack }) => {
-  const [listDetails, setListDetails] = useState< (Item[] | [] )>([]);
+  const [listDetails, setListDetails] = useState<Item[]>([]);
   const [error, setError] = useState<string | null>(null);
   const [change, setChange] = useState<boolean>(false);
- console.log('listdeets', listDetails)
+
   const fetchListDetails = async () => {
     try {
-      const response = await fetch(`https://closet-manager-be.herokuapp.com/api/v1/users/1/lists/${listId}/items`);
+      const response = await fetch(
+        `https://closet-manager-be.herokuapp.com/api/v1/users/1/lists/${listId}/items`
+      );
+
       if (!response.ok) {
-        throw new Error('Failed to fetch list details');
+        throw new Error("Failed to fetch list details");
       }
+
       const data = await response.json();
       const items = data.data;
       setListDetails(items);
     } catch (error) {
-      setError('An error occurred while fetching the list details.');
+      setError("An error occurred while fetching the list details.");
     }
   };
-
-
 
   useEffect(() => {
     fetchListDetails();
@@ -54,34 +55,40 @@ export const List: React.FC<ListProps> = ({ listId, onBack }) => {
 
   const handleDelete = async (itemId: string) => {
     try {
-      const response = await fetch(`https://closet-manager-be.herokuapp.com/api/v1/lists/${itemId}/lists/${itemId}`, {
-        method: 'DELETE'
-      });
+      const response = await fetch(
+        `https://closet-manager-be.herokuapp.com/api/v1/lists/${itemId}/lists/${itemId}`,
+        {
+          method: "DELETE",
+        }
+      );
+
       if (!response.ok) {
-        throw new Error('Failed to delete item');
+        throw new Error("Failed to delete item");
       }
+
       setChange(!change);
     } catch (error) {
-      setError('An error occurred while deleting the item.');
+      setError("An error occurred while deleting the item.");
     }
   };
+
   return (
     <div>
-      <button className="list-back-btn" onClick={handleBack}>Back</button>
-      {error && 
-        <h2>{error}</h2>}
-        <>
-          {/* <h2>{listDetails}</h2> */}
-          <div className='card-grid'>
-            {listDetails && listDetails.map((item: Item) => (
-              <div key={item.id}>
-                <Card id={item.id} image={item.attributes.image_url} setChange={setChange} />
-                <button onClick={() => handleDelete(item.id)}>Delete</button>
-              </div>
-            ))}
-          </div>
-        </>
-      
+      <button className="list-back-btn" onClick={handleBack}>
+        Back
+      </button>
+
+      {error && <h2>{error}</h2>}
+
+      <div className="card-grid">
+        {listDetails &&
+          listDetails.map((item: Item) => (
+            <div key={item.id}>
+              <Card id={item.id} image={item.attributes.image_url} setChange={setChange} />
+              <button onClick={() => handleDelete(item.id)}>Delete</button>
+            </div>
+          ))}
+      </div>
     </div>
-  );  
-};  
+  );
+};
