37db992d3ef30f541b5aeb6f0f7327d9d32898da
Blanche Haddad <blancherachel@gmail.com>
2023-04-06 08:25:33 -0600

    fix: Import image from assets

--- src/Components/PageNotFound/PageNotFound.tsx
+++ src/Components/PageNotFound/PageNotFound.tsx
@@ -1,5 +1,6 @@
 import "./PageNotFound.css"
 import { NavLink } from "react-router-dom";
+import image from "../../assets/empty-closet.png"
 
 export const PageNotFound = (): JSX.Element => {
   return (
@@ -8,7 +9,7 @@ export const PageNotFound = (): JSX.Element => {
       <NavLink to={"/"}>
         <button className="page-not-found-btn">Return Home</button>
       </NavLink>
-      <img className="empty-closet-image" src="src/assets/empty-closet.png" alt="Picture of an empty closet"></img>
+      <img className="empty-closet-image" src={image} alt="Picture of an empty closet"></img>
     </section>
   )
 }
