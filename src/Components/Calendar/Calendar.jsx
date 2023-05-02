c227dc067f80905986a95666ecc6df0b361a5691
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-05-02 13:09:53 -0500

    Change popperPlacement

--- src/Components/Calendar/Calendar.jsx
+++ src/Components/Calendar/Calendar.jsx
@@ -1,4 +1,4 @@
-import DatePicker from "react-date-picker";
+import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { useState } from "react";
 
@@ -6,9 +6,10 @@ export const Calendar = () => {
   const [startDate, setStartDate] = useState(new Date());
   return (
     <DatePicker
+      showIcon
       selected={startDate}
       onChange={(date) => setStartDate(date)}
-      inline
+      popperPlacement="top"
     />
   );
 };
