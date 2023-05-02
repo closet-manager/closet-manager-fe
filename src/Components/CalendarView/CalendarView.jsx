2be61923d711db881b26cde716487f3557d93b3a
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-05-02 15:19:27 -0500

    add navigation to the date view when a user clicks on a date

--- src/Components/CalendarView/CalendarView.jsx
+++ src/Components/CalendarView/CalendarView.jsx
@@ -1,24 +1,34 @@
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { useState } from "react";
-import { addToCalendar } from "../../apiCall";
+import "./CalendarView.css"
+import { useNavigate, useParams } from "react-router-dom";
 
 export const CalendarView = () => {
+  const navigate = useNavigate();
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(null);
+
   const onChange = (dates) => {
     const [start, end] = dates;
     setStartDate(start);
     setEndDate(end);
   };
+
   return (
-    <DatePicker
-      selected={startDate}
-      onChange={onChange}
-      startDate={startDate}
-      endDate={endDate}
-      selectsRange
-      inline
-    />
+    <div className="calendar-container" >
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => {
+            if (date) {
+              const selectedDate = new Date(date).toISOString().slice(0, 10)
+              console.log(selectedDate);
+              navigate(`/date/${selectedDate}`)
+            }
+          }}
+          selectsRange
+          inline
+        />
+    </div>
   );
 }
