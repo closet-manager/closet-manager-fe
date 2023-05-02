9f9b395ff2efeb8117d52ba847754c2d22b2539e
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-05-02 13:28:42 -0500

    Create getDate function

--- src/Components/Calendar/Calendar.jsx
+++ src/Components/Calendar/Calendar.jsx
@@ -1,15 +1,25 @@
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { useState } from "react";
+import { getSingleItem, deleteItem } from "../../apiCall";
 
-export const Calendar = () => {
+export const Calendar = ({ id }) => {
   const [startDate, setStartDate] = useState(new Date());
+
+const getDate = (date) => {
+  console.log(date.toISOString().slice(0, 10))
+  console.log(id)
+
+}
+
   return (
     <DatePicker
       showIcon
       selected={startDate}
-      onChange={(date) => setStartDate(date)}
-      popperPlacement="top"
+      onChange={(date) => {
+        setStartDate(date) 
+        getDate(date)}}
+        popperPlacement="top"
     />
   );
 };
