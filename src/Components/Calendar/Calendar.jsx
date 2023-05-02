f9474c66538eee9b31d1938b486e31239e7190d8
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-05-02 14:10:07 -0500

    Change text when an item is successfully added

--- src/Components/Calendar/Calendar.jsx
+++ src/Components/Calendar/Calendar.jsx
@@ -1,25 +1,33 @@
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { useState } from "react";
-import { getSingleItem, deleteItem } from "../../apiCall";
+import { addToCalendar } from "../../apiCall";
 
-export const Calendar = ({ id }) => {
+export const Calendar = ({ id, setCalText }) => {
   const [startDate, setStartDate] = useState(new Date());
 
 const getDate = (date) => {
   console.log(date.toISOString().slice(0, 10))
   console.log(id)
-
+  addToCalendar(date.toISOString().slice(0, 10), id)
+  .then((res) => {
+    if (res.ok) {
+      setCalText("Added to Calendar!")
+    }
+    else {
+      setCalText("Error - Unable to Add to Calendar")
+    }
+  })
 }
 
   return (
     <DatePicker
       showIcon
+      popperPlacement="top"
       selected={startDate}
       onChange={(date) => {
         setStartDate(date) 
         getDate(date)}}
-        popperPlacement="top"
     />
   );
 };
