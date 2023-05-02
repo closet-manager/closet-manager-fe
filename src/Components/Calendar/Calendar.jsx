c45021dea6da5bce4265bec94909144a187f4bd1
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-05-02 11:50:34 -0500

    Create the calendar component

---
+++ src/Components/Calendar/Calendar.jsx
@@ -0,0 +1,15 @@
+import DatePicker from "react-date-picker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useEffect, useState } from "react";
+
+export const Calendar = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  return (
+    <DatePicker
+      selected={startDate}
+      onChange={(date) => setStartDate(date)}
+      inline
+    />
+  );
+};
+
