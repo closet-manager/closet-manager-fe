2ad5d4f6a087a9f839e2da6c4ab30fdb512ad86e
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-05-03 12:11:38 -0500

    Add markers on the calendar for dates with items

--- src/Components/CalendarView/CalendarView.jsx
+++ src/Components/CalendarView/CalendarView.jsx
@@ -1,13 +1,17 @@
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./CalendarView.css"
 import { useNavigate } from "react-router-dom";
+import { getAllDates } from "../../apiCall";
 
 export const CalendarView = () => {
   const navigate = useNavigate();
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(null);
+  const [dates, setDates] = useState(undefined)
+  const [error, setError] = useState(undefined);
+  const [marker, setMarker] = useState(undefined)
 
   const onChange = (dates) => {
     const [start, end] = dates;
@@ -15,20 +19,40 @@ export const CalendarView = () => {
     setEndDate(end);
   };
 
+  useEffect(() => {
+    getAllDates()
+    .then((res) => {
+      setDates(res.data.map((date) => date.attributes["outfit_date"]))
+    
+      setMarker(dates.map((day) => {
+        // console.log(day)
+        let newDate = new Date(day)
+          return new Date(newDate.getTime() + Math.abs(newDate.getTimezoneOffset()*60000) )  
+        }))
+    
+    })
+    .catch((err) => setError(err))
+  }, [dates])
+
+
   return (
     <div className="calendar-container" >
-        <DatePicker
+       {dates && marker && <DatePicker
           selected={startDate}
           onChange={(date) => {
             if (date) {
+              console.log(date)
               const selectedDate = new Date(date).toISOString().slice(0, 10)
-              console.log(selectedDate);
               navigate(`/date/${selectedDate}`)
             }
           }}
           selectsRange
           inline
-        />
+          hightlightDates={[]}
+          highlightDates={marker}
+         
+          
+        />}
     </div>
   );
 }
