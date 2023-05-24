4690d162ad010cdf457ee9e9f11c0d88c9a85794
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-05-24 15:56:48 -0500

    Create Cypress spec for the date view

---
+++ cypress/e2e/calendar-date-view.cy.ts
@@ -0,0 +1,15 @@
+describe("date view", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:5173/calendar");
+    cy.intercept("GET", "https://closet-manager-be.herokuapp.com/api/v1/events", {"data":[{"id":"3","type":"event","attributes":{"outfit_date":"2023-05-11"}},{"id":"4","type":"event","attributes":{"outfit_date":"2023-05-22"}},{"id":"5","type":"event","attributes":{"outfit_date":"2023-05-14"}},{"id":"6","type":"event","attributes":{"outfit_date":"2023-05-08"}}]})
+    cy.intercept("GET", "http://localhost:5173/__cypress/assets/favicon.png?v2", {})
+    cy.wait(1000)
+    cy.get("select").eq(0).select(4)
+    cy.get("select").eq(1).select(123)
+    cy.get("div").eq(30).click()
+  });
+  // it("Should display the date", () => {
+  //   cy.get("h2").should("have.text", "May 8th, 2023")
+  // });
+  
+});
