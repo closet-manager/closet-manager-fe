1c42e5a033ddb88f0569dea484bcd972c1f59cff
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-05-24 16:05:42 -0500

    Stub the netwrok request for getting all items added to a date in the Cypress test

--- cypress/e2e/calendar-date-view.cy.ts
+++ cypress/e2e/calendar-date-view.cy.ts
@@ -1,15 +1,14 @@
 describe("date view", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:5173/calendar");
-    cy.intercept("GET", "https://closet-manager-be.herokuapp.com/api/v1/events", {"data":[{"id":"3","type":"event","attributes":{"outfit_date":"2023-05-11"}},{"id":"4","type":"event","attributes":{"outfit_date":"2023-05-22"}},{"id":"5","type":"event","attributes":{"outfit_date":"2023-05-14"}},{"id":"6","type":"event","attributes":{"outfit_date":"2023-05-08"}}]})
+    cy.visit("http://localhost:5173/date/2023-05-08");
+    cy.intercept("GET", "https://closet-manager-be.herokuapp.com/api/v1/event_items/find_all?date=2023-05-08", {"data":[{"id":"39","type":"item","attributes":{"user_id":1,"season":"all_season","clothing_type":"accessories","color":"neutral","size":"","image_url":"https://closet-manager-be.herokuapp.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBMQT09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--ae3a8ce6ac5aa716c3475265c9e90e5f22992e9c/image.jpg","notes":"","favorite":false}}]})
     cy.intercept("GET", "http://localhost:5173/__cypress/assets/favicon.png?v2", {})
-    cy.wait(1000)
-    cy.get("select").eq(0).select(4)
-    cy.get("select").eq(1).select(123)
-    cy.get("div").eq(30).click()
   });
   it("Should display the date", () => {
     cy.get("h2").should("have.text", "May 8th, 2023")
+  });
+  it("Should display any items saved for the date", () => {
+    cy.get("img[class='card-image']")
   });
   
 });
